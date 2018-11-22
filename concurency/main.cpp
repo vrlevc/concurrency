c92fdaf164337d718667cd9e2a16ebc1568fd7c4
Vitya <levchenko.victor@gmail.com>
2018-11-22 17:04:33 +0200

    Initial data for statring project

---
+++ concurency/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  concurency
+//
+//  Created by Viktor Levchenko on 11/22/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
