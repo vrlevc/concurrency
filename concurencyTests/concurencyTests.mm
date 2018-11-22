7a553c659c8fe87c5fa6aa173aa81bc196011cd2
Vitya <levchenko.victor@gmail.com>
2018-11-22 17:07:26 +0200

    Initial unit test target and initial test in it

---
+++ concurencyTests/concurencyTests.mm
@@ -0,0 +1,37 @@
+//
+//  concurencyTests.m
+//  concurencyTests
+//
+//  Created by Viktor Levchenko on 11/22/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface concurencyTests : XCTestCase
+
+@end
+
+@implementation concurencyTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
