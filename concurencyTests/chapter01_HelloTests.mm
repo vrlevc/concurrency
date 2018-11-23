19ed4859c727e016ef8b02bf70e09f416c736b8d
Vitya <levchenko.victor@gmail.com>
2018-11-23 09:22:46 +0200

    Initial unit test for chapter 01 : Hello, world of concurrency in C++!

---
+++ concurencyTests/chapter01_HelloTests.mm
@@ -0,0 +1,37 @@
+//
+//  chapter01_HelloTests.m
+//  concurencyTests
+//
+//  Created by Viktor Levchenko on 11/23/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface chapter01_HelloTests : XCTestCase
+
+@end
+
+@implementation chapter01_HelloTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
