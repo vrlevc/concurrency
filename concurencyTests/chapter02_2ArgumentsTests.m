81ee700f981d7b8cc340ed3d9088950b0fcc983d
vitya <levchenko.victor@gmail.com>
2018-11-24 14:30:11 +0200

    initial tests file for chapter 2.2 - passing arguments to the thread

---
+++ concurencyTests/chapter02_2ArgumentsTests.m
@@ -0,0 +1,37 @@
+//
+//  chapter02_2ArgumentsTests.m
+//  concurencyTests
+//
+//  Created by lva on 11/24/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface chapter02_2ArgumentsTests : XCTestCase
+
+@end
+
+@implementation chapter02_2ArgumentsTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
