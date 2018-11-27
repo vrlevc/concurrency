ad2cb7436b8cd265c80a5c56cdb6ac0f616336bc
Vitya <levchenko.victor@gmail.com>
2018-11-27 12:36:41 +0200

    initial test file template

--- concurencyTests/chapter02_5IdentifyingTests.mm
+++ concurencyTests/chapter02_5IdentifyingTests.mm
@@ -8,30 +8,17 @@
 
 #import <XCTest/XCTest.h>
 
+// MARK: -
+
 @interface chapter02_5IdentifyingTests : XCTestCase
-
 @end
-
 @implementation chapter02_5IdentifyingTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
+// MARK: -
 
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
 }
 
 @end
