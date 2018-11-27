7ffca6756c2c9281a9e98dbb24257655e5c98c19
Vitya <levchenko.victor@gmail.com>
2018-11-27 09:24:28 +0200

    prepare 2.2 test file ...

--- concurencyTests/chapter02_4ThreadsNumberTests.mm
+++ concurencyTests/chapter02_4ThreadsNumberTests.mm
@@ -8,30 +8,21 @@
 
 #import <XCTest/XCTest.h>
 
+// MARK: -
+
 @interface chapter02_4ThreadsNumberTests : XCTestCase
 
 @end
 
 @implementation chapter02_4ThreadsNumberTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+// MARK: -
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void)testExample {
+- (void)testExample
+{
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
