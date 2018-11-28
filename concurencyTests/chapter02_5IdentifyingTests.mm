7ce3bf468c2618e8d5ea2de8011dfd6612ffe420
vitya <levchenko.victor@gmail.com>
2018-11-28 21:40:21 +0200

    test thread get_id() function

--- concurencyTests/chapter02_5IdentifyingTests.mm
+++ concurencyTests/chapter02_5IdentifyingTests.mm
@@ -5,6 +5,15 @@
 
 #import <XCTest/XCTest.h>
 
+#include <vector>
+#include <thread>
+#include <iostream>
+
+static void printThreadId()
+{
+    std::cout << "  >>> thread id : " << std::this_thread::get_id() << std::endl;
+}
+
 // MARK: -
 
 @interface chapter02_5IdentifyingTests : XCTestCase
@@ -13,10 +22,16 @@
 
 // MARK: -
 
-- (void)testExample
+- (void)testThreadId
 {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    std::vector<std::thread> pull;
+    for (int i=0;i<10;++i)
+        pull.push_back( std::thread( printThreadId ) );
+    
+    std::for_each(pull.begin(), pull.end(), [](std::thread& t){
+       std::cout << "  >>> thread id : " << t.get_id() << std::endl;
+    });
+    std::for_each(pull.begin(), pull.end(), std::mem_fn(&std::thread::join));
 }
 
 @end
