cefd17d06325b40c17b22aa250aae0fb2ce57911
Vitya <levchenko.victor@gmail.com>
2018-12-04 11:14:59 +0200

    make file c++ compatible ...

---
+++ concurencyTests/chapter03_2_3ThreadSafeIterfaceTests.mm
@@ -0,0 +1,24 @@
+//
+//  chapter03_2_3ThreadSafeIterfaceTests.m
+//  concurencyTests
+//
+//  Created by Viktor Levchenko on 12/4/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+// MARK: -
+
+@interface chapter03_2_3ThreadSafeIterfaceTests : XCTestCase
+@end
+
+@implementation chapter03_2_3ThreadSafeIterfaceTests
+
+// MARK: -
+
+- (void)testExample
+{
+}
+
+@end
