c3a5fb3a780f7e5ebedc3db03bda6fcad5095d40
Vitya <levchenko.victor@gmail.com>
2018-12-04 11:34:56 +0200

    threadsafe_stack implementation and allocation test

--- concurencyTests/chapter03_2_3ThreadSafeIterfaceTests.mm
+++ concurencyTests/chapter03_2_3ThreadSafeIterfaceTests.mm
@@ -8,6 +8,66 @@
 
 #import <XCTest/XCTest.h>
 
+#include <exception>
+#include <memory>		// for std::shared_ptr
+#include <stack>
+#include <mutex>
+
+/// Listing 3.4 An outline class definition for a thread-safe stack
+
+struct empty_stack : std::exception
+{
+	const char* what() const noexcept;
+};
+
+template<typename T>
+class threadsafe_stack
+{
+private:
+	std::stack<T> data;
+	mutable std::mutex m;
+public:
+	threadsafe_stack() {}
+	threadsafe_stack(const threadsafe_stack& other)
+	{
+		// copy preformed in constructor body
+		std::lock_guard<std::mutex> lock(other.m);
+		data = other.data;
+	}
+	threadsafe_stack& operator=(const threadsafe_stack&) = delete; // assignment operator is deleted
+	
+	void push(T new_value)
+	{
+		std::lock_guard<std::mutex> lock(m);
+		data.push(new_value);
+	}
+	std::shared_ptr<T> pop()	// return top as a pointer, throws exception if empty
+	{
+		std::lock_guard<std::mutex> lock(m);
+		
+		/// Check for empty before trying to pop value:
+		if (data.empty()) throw empty_stack();
+		
+		/// Allocate return value before modify stack:
+		std::shared_ptr<T> const res( std::make_shared<T>(data.top()) );
+		data.pop();
+		
+		return res;
+	}
+	void pop(T& value)			// put top into referenced value, throws exception if empty
+	{
+		std::lock_guard<std::mutex> lock(m);
+		if (data.empty()) throw empty_stack();
+		value = data.top();
+		data.pop();
+	}
+	bool empty() const
+	{
+		std::lock_guard<std::mutex> lock(m);
+		return data.empty();
+	}
+};
+
 // MARK: -
 
 @interface chapter03_2_3ThreadSafeIterfaceTests : XCTestCase
@@ -17,8 +77,10 @@
 
 // MARK: -
 
-- (void)testExample
+- (void)testThreadSafeStack
 {
+	// Allocate test:
+	threadsafe_stack<int> some_stack;	
 }
 
 @end
