3c7eeffd898beb0be15ec5394fe75ce7697ecb2a
Vitya <levchenko.victor@gmail.com>
2018-12-05 13:11:57 +0200

    initial file for chapter 3.2.4. Deadlocks

---
+++ concurencyTests/chapter03_2_4DeadlockTests.mm
@@ -0,0 +1,24 @@
+//
+//  chapter03_2_4DeadlockTests.m
+//  concurencyTests
+//
+//  Created by Viktor Levchenko on 12/5/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+// MARK: -
+
+@interface chapter03_2_4DeadlockTests : XCTestCase
+@end
+
+@implementation chapter03_2_4DeadlockTests
+
+// MARK: -
+
+- (void)testExample
+{
+}
+
+@end
