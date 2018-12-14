d1df0add2471d19b582feebc9044a6a9dc94775e
Vitya <levchenko.victor@gmail.com>
2018-12-14 14:43:17 +0200

    initial file for chapter 3.3. Alternative facilities for protecting shared data

---
+++ concurencyTests/c03_3_AlternativeLocksTests.mm
@@ -0,0 +1,25 @@
+//
+//  c03_3_AlternativeLocksTests.m
+//  concurencyTests
+//
+//  Created by Viktor Levchenko on 12/14/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+// MARK: -
+
+@interface c03_3_AlternativeLocksTests : XCTestCase
+@end
+
+@implementation c03_3_AlternativeLocksTests
+
+// MARK: -
+
+- (void)testExample
+{
+}
+
+
+@end
