30db24cad0890699e44cbb32518549dae0dddffb
Vitya <levchenko.victor@gmail.com>
2018-12-17 11:09:21 +0200

    Lazy initialization - thread safe by std::call_once and std::once_falg

--- concurencyTests/c03_3_AlternativeLocksTests.mm
+++ concurencyTests/c03_3_AlternativeLocksTests.mm
@@ -53,6 +53,10 @@ struct some_resource_t
 	std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
 }
 
+/// ------------------------------- ///
+/// std::once_flag & std::call_once ///
+/// ------------------------------- ///
+
 - (void)testLazyInitialization_GOOD
 {
 	std::shared_ptr<some_resource_t> resource_ptr;
@@ -63,7 +67,7 @@ struct some_resource_t
 	};
 	auto foo = [&]()
 	{
-		/// Initialization is called exactly once 
+		/// Initialization is called exactly once
 		std::call_once(resource_flag, init_resource);
 		resource_ptr->do_something();
 	};
@@ -72,6 +76,96 @@ struct some_resource_t
 	for (int i=0;i<10;++i)
 		threads.emplace_back(foo);
 	std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
+}
+
+/// Listing 3.12 Thread-safe lazy initialization of a class member using std::call_once
+
+- (void)testLazyInitialization_class
+{
+	/// 1: External non thread safe function to init connection:
+	using data_packet = std::string;
+	class Connection
+	{
+	private:
+		std::string trafic;
+	public:
+		Connection() = default;
+		Connection(Connection const &) = default;
+		Connection(std::string const & connection_data) : trafic(connection_data) {}
+		void send_data(data_packet const & data)
+		{
+			// non thread safe - need one ...
+		}
+		data_packet recieve_data()
+		{
+			return "Some data package";
+		}
+	};
+	class ConnectionLogger
+	{
+	public:
+		Connection connect(std::string const & info)
+		{
+			std::string connectionLog = "Connected to : ";
+			connectionLog.append( info );
+			Connection connection(connectionLog);
+			return connection;
+		};
+	};
+	// 2: Test class - used thread safe lazy init
+	class X
+	{
+	private:
+		using connection_info = std::string;
+		using connection_handle = Connection;
+		using data_packet = std::string;
+		
+		ConnectionLogger connection_manager;
+		connection_info connection_details;
+		connection_handle connection;
+		std::once_flag connection_init_flag;	// data guard
+		
+		/// 3: non thread safe!!!
+		void open_connection()
+		{
+			connection=connection_manager.connect(connection_details);
+		}
+	public: /// 4: thread safe public interface:
+		X(connection_info const & connection_detatils_)	: connection_details(connection_detatils_) {}
+		void send_data(data_packet const & data)
+		{
+			std::call_once(connection_init_flag, &X::open_connection, this);	// guard class function
+			connection.send_data(data);
+		}
+		data_packet recieve_data()
+		{
+			std::call_once(connection_init_flag, &X::open_connection, this);
+			return connection.recieve_data();
+		}
+	};
+	
+	// 3: Using class X for send and receive data:
+	auto server_handler = []()
+	{
+		X server("mail.server.com");
+		for (int i=0;i<10;++i)
+		{
+			server.send_data("send e-mails");
+			server.recieve_data();
+		}
+	};
+	
+	// 4: create multy threaded server with one connection:
+	X sender("sender.server.com");
+	X receiver("receiver.server.com");
+	std::vector<std::thread> users;
+	for (int i=0;i<50;++i)
+	{
+		users.emplace_back(server_handler);
+		users.emplace_back(&X::send_data, &sender, "send by sender");
+		users.emplace_back(&X::recieve_data, &receiver);
+	}
+	std::for_each(users.begin(), users.end(), std::mem_fn(&std::thread::join));
 }
 
 @end
