8a28a313526a0d6ca20eefa8a0fd8c44e54a2666
Vitya <levchenko.victor@gmail.com>
2018-12-20 09:50:00 +0200

    condition variables tests initial file

---
+++ concurencyTests/c04_1_ConditionVariablesTests.mm
@@ -0,0 +1,24 @@
+//
+//  c04_1_ConditionVariablesTests.m
+//  concurencyTests
+//
+//  Created by Viktor Levchenko on 12/20/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+// MARK: -
+
+@interface c04_1_ConditionVariablesTests : XCTestCase
+@end
+
+@implementation c04_1_ConditionVariablesTests
+
+// MARK: -
+
+- (void)testExample
+{
+}
+
+@end
