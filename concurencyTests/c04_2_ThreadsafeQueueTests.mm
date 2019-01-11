8838a806fe99dfe90cbd80ef9c0ab01cf711a420
Vitya <levchenko.victor@gmail.com>
2019-01-11 10:39:59 +0200

    finish threadsafe_queue class extraction

--- concurencyTests/c04_2_ThreadsafeQueueTests.mm
+++ concurencyTests/c04_2_ThreadsafeQueueTests.mm
@@ -19,10 +19,17 @@ template<typename T>
 class threadsafe_queue
 {
 private:
-	std::mutex mut;
+	mutable std::mutex mut;		/// The mutex must be mutable
 	std::queue<T> data_queue;
 	std::condition_variable data_cond;
 public:
+	threadsafe_queue()
+	{}
+	threadsafe_queue(threadsafe_queue const& other)
+	{
+		std::lock_guard<std::mutex> lk(other.mut);
+		data_queue = other.data_queue;
+	}
 	void push(T new_value)
 	{
 		std::lock_guard<std::mutex> lk(mut); // 2.1: protect queue
@@ -35,6 +42,37 @@ public:
 		data_cond.wait(lk, [this]{ return !data_queue.empty(); });
 		value=data_queue.front();
 		data_queue.pop();
+	}
+	std::shared_ptr<T> wait_and_pop()
+	{
+		std::unique_lock<std::mutex> lk(mut);
+		data_cond.wait(lk, [this]{ return !data_queue.empty(); });
+		std::shared_ptr<T> res( std::make_shared<T>(data_queue.front()) );
+		data_queue.pop();
+		return res;
+	}
+	bool try_pop(T& value)
+	{
+		std::lock_guard<std::mutex> lk(mut);
+		if (data_queue.empty())
+			return false;
+		value=data_queue.front();
+		data_queue.pop();
+		return true;
+	}
+	std::shared_ptr<T> try_pop()
+	{
+		std::lock_guard<std::mutex> lk(mut);
+		if (data_queue.empty())
+			return std::shared_ptr<T>();
+		std::shared_ptr<T> res( std::make_shared<T>(data_queue.front()) );
+		data_queue.pop();
+		return res;
+	}
+	bool empty() const
+	{
+		std::lock_guard<std::mutex> lk(mut);
+		return data_queue.empty();
 	}
 };
 
