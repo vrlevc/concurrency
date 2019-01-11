d1b7b18db8c39cdfa6654834ec46676c55898d99
Vitya <levchenko.victor@gmail.com>
2019-01-11 10:51:28 +0200

    rename test to its chapter name

--- concurencyTests/c04_1_2ThreadsafeQueueTests.mm
+++ concurencyTests/c04_1_2ThreadsafeQueueTests.mm
@@ -8,75 +8,11 @@
 
 #import <XCTest/XCTest.h>
 
+#include "threadsafe_queue.hpp"
+
 #include <string>
 #include <list>
-#include <queue>
-#include <mutex>
-#include <condition_variable>
 #include <thread>
-
-template<typename T>
-class threadsafe_queue
-{
-private:
-	mutable std::mutex mut;		/// The mutex must be mutable
-	std::queue<T> data_queue;
-	std::condition_variable data_cond;
-public:
-	threadsafe_queue()
-	{}
-	threadsafe_queue(threadsafe_queue const& other)
-	{
-		std::lock_guard<std::mutex> lk(other.mut);
-		data_queue = other.data_queue;
-	}
-	void push(T new_value)
-	{
-		std::lock_guard<std::mutex> lk(mut); // 2.1: protect queue
-		data_queue.push(new_value);	// 2.2: push data into queue
-		data_cond.notify_one(); //   3: notify waiting threads
-	}
-	void wait_and_pop(T& value)
-	{
-		std::unique_lock<std::mutex> lk(mut);
-		data_cond.wait(lk, [this]{ return !data_queue.empty(); });
-		value=data_queue.front();
-		data_queue.pop();
-	}
-	std::shared_ptr<T> wait_and_pop()
-	{
-		std::unique_lock<std::mutex> lk(mut);
-		data_cond.wait(lk, [this]{ return !data_queue.empty(); });
-		std::shared_ptr<T> res( std::make_shared<T>(data_queue.front()) );
-		data_queue.pop();
-		return res;
-	}
-	bool try_pop(T& value)
-	{
-		std::lock_guard<std::mutex> lk(mut);
-		if (data_queue.empty())
-			return false;
-		value=data_queue.front();
-		data_queue.pop();
-		return true;
-	}
-	std::shared_ptr<T> try_pop()
-	{
-		std::lock_guard<std::mutex> lk(mut);
-		if (data_queue.empty())
-			return std::shared_ptr<T>();
-		std::shared_ptr<T> res( std::make_shared<T>(data_queue.front()) );
-		data_queue.pop();
-		return res;
-	}
-	bool empty() const
-	{
-		std::lock_guard<std::mutex> lk(mut);
-		return data_queue.empty();
-	}
-};
-
-// MARK: -
 
 struct raw_data_t
 {
@@ -93,10 +29,10 @@ static bool is_last_chunk(data_chunk& chunk);
 
 // MARK: -
 
-@interface c04_2_ThreadsafeQueueTests : XCTestCase
+@interface c04_1_2ThreadsafeQueueTests : XCTestCase
 @end
 
-@implementation c04_2_ThreadsafeQueueTests
+@implementation c04_1_2ThreadsafeQueueTests
 
 
 - (void)setUp
