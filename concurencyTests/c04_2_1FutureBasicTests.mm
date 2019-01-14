21d80f277737a0ab07de71a1fa76b466b997cf36
Vitya <levchenko.victor@gmail.com>
2019-01-14 09:51:21 +0200

    std::future basic initial test file for chapter 4.2.1

---
+++ concurencyTests/c04_2_1FutureBasicTests.mm
@@ -0,0 +1,25 @@
+//
+//  c04_2_1FutureBasicTests.m
+//  concurencyTests
+//
+//  Created by Viktor Levchenko on 1/14/19.
+//  Copyright © 2019 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+// MARK: -
+
+@interface c04_2_1FutureBasicTests : XCTestCase
+@end
+
+@implementation c04_2_1FutureBasicTests
+
+// MARK: -
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
