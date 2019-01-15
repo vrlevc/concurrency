be1d32235f940f23dc5070f7f25f5c981a6c6ad4
Vitya <levchenko.victor@gmail.com>
2019-01-15 09:13:50 +0200

    initial test for std::packaged_task usage

---
+++ concurencyTests/c04_2_2PackagedTaskTests.mm
@@ -0,0 +1,25 @@
+//
+//  c04_2_2PackagedTaskTests.m
+//  concurencyTests
+//
+//  Created by Viktor Levchenko on 1/15/19.
+//  Copyright © 2019 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+// MARK: -
+
+@interface c04_2_2PackagedTaskTests : XCTestCase
+@end
+
+@implementation c04_2_2PackagedTaskTests
+
+// MARK: -
+
+- (void)testExample
+{
+	
+}
+
+@end
