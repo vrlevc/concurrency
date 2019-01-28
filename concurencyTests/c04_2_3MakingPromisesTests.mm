9edfa18497532e4b300dff59239ccf0f326c1fc2
Vitya <levchenko.victor@gmail.com>
2019-01-28 18:49:15 +0200

    Initial unit tests file for Chapter 4.2.3 Making (std::)promises

---
+++ concurencyTests/c04_2_3MakingPromisesTests.mm
@@ -0,0 +1,34 @@
+//
+//  c04_2_3MakingPromisesTests.m
+//  concurencyTests
+//
+//  Created by Viktor Levchenko on 1/28/19.
+//  Copyright © 2019 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+// MARK: -
+
+@interface c04_2_3MakingPromisesTests : XCTestCase
+@end
+
+@implementation c04_2_3MakingPromisesTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+// MARK: -
+
+- (void)testExample
+{
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
