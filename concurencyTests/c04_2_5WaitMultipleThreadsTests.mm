71717cc7cc6ab286dbc20042125504cd73a7fc40
vitya <levchenko.victor@gmail.com>
2019-03-13 09:27:17 +0200

    Just a new file for chapter 4.2.5 - waiting multiple threads by shared async

---
+++ concurencyTests/c04_2_5WaitMultipleThreadsTests.mm
@@ -0,0 +1,26 @@
+//
+//  c04_2_5WaitMultipleThreadsTests.m
+//  concurencyTests
+//
+//  Created by lva on 3/13/19.
+//  Copyright © 2019 LVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+// MARK: -
+
+@interface c04_2_5WaitMultipleThreadsTests : XCTestCase
+@end
+
+@implementation c04_2_5WaitMultipleThreadsTests
+
+// MARK: -
+
+- (void)testExample
+{
+}
+
+@end
+
+// MARK: -
